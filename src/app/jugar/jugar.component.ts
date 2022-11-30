059e25274c1065792f31fece5cff81c68f8b3ad2
Emilioj3sus <emiliojesus.a@hotmail.com>
2022-11-30 11:34:27 -0300

    pagina en blanco

---
+++ src/app/jugar/jugar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jugar',
+  templateUrl: './jugar.component.html',
+  styleUrls: ['./jugar.component.css']
+})
+export class JugarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("que pasa")
+  }
+
+
+}
